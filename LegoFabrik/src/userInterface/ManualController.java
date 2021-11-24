239cee9e7a3f4883164a0bc211a7a5ff213a6c8d
DESKTOP-2Q8PMPB\roman <roman.e.schulz@web.de>
2021-11-24 12:16:00 +0100

    Prepared code for new buttons, not implemented yet

--- LegoFabrik/src/userInterface/ManualController.java
+++ LegoFabrik/src/userInterface/ManualController.java
@@ -858,7 +858,7 @@ public class ManualController implements Initializable {
 	}
 
 	// --------------------------------------------------------------------
-	// Airarms
+	// Airarms Schalter 1
 	public void airarmsVorButtonPressed1() {
 		try {
 			s.getAirarms().getMoveArm().forward();
@@ -895,7 +895,7 @@ public class ManualController implements Initializable {
 	}
 
 	// --------------------------------------------------------------------
-	// Airarms
+	// Airarms Schalter 2
 	public void airarmsVorButtonPressed12() {
 		try {
 			s.getAirarms().getVerticalArm().forward();
@@ -931,7 +931,7 @@ public class ManualController implements Initializable {
 	}
 
 	// --------------------------------------------------------------------
-	// Airarms
+	// Airarms Schalter 3
 	public void airarmsVorButtonPressed13() {
 		try {
 			s.getAirarms().getTurnGrab().forward();
@@ -967,7 +967,7 @@ public class ManualController implements Initializable {
 	}
 
 	// --------------------------------------------------------------------
-	// Airarms
+	// Airarms Schalter 4
 	public void airarmsVorButtonPressed14() {
 		try {
 			s.getAirarms().getOpenCloseGrab().forward();
@@ -1001,9 +1001,44 @@ public class ManualController implements Initializable {
 			e.printStackTrace();
 		}
 	}
+	// --------------------------------------------------------------------
+	// Airarms Schalter 5 TODO
+	public void airarmsVorButtonPressed17() {
+		try {
+			s.getAirarms().getOpenCloseGrab().forward();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void airarmsVorButtonReleased17() {
+		try {
+			s.getAirarms().getOpenCloseGrab().stop(false);
+			System.out.println(s.getAirarms().getOpenCloseGrab().getTachoCount());
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void airarmsBackButtonPressed17() {
+		try {
+			s.getAirarms().getOpenCloseGrab().backward();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void airarmsBackButtonReleased17() {
+		try {
+			s.getAirarms().getOpenCloseGrab().stop(false);
+			System.out.println(s.getAirarms().getOpenCloseGrab().getTachoCount());
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
 
 	// --------------------------------------------------------------------
-	// Airarms
+	// Airarms Dreharm
 	public void airarmsVorButtonPressed15() {
 		try {
 			s.getAirarms().getTurnArm1().forward();
@@ -1039,7 +1074,7 @@ public class ManualController implements Initializable {
 	}
 
 	// --------------------------------------------------------------------
-	// Airarms
+	// Airarms Dreharm
 	public void airarmsVorButtonPressed16() {
 		try {
 			s.getAirarms().getTurnArm2().forward();
